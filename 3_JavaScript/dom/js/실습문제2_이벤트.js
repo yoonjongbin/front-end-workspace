03d34ed8edb394da9bbbe04c6293331348b906a2
yoonjongbin <133620288+yoonjongbin@users.noreply.github.com>
2023-07-25 17:50:49 +0900

    11 days

--- 3_JavaScript/dom/js/실습문제2_이벤트.js
+++ 3_JavaScript/dom/js/실습문제2_이벤트.js
@@ -23,3 +23,73 @@ window.addEventListener('scroll', function(){
 window.addEventListener('resize', resizeHandler());
 
 resizeHandler();
+
+
+// navigation click event
+
+const nav = document.querySelector('nav');
+let current;
+
+function navHandler(e){
+    
+    if(current){
+        current.style.backgroundColor = 'transparent';
+        current.style.color = 'black';
+    }
+
+    if(e.target !== e.currentTarget){
+        e.target.style.backgroundColor = 'black';
+        e.target.style.color = 'white';
+        current = e.target;
+    }
+    // console.log(e.target);
+    
+}
+
+nav.addEventListener('click', navHandler);
+
+
+// mouser wheel event
+
+const section1 = document.querySelector('#section1');
+const section2 = document.querySelector('#section2');
+const section3 = document.querySelector('#section3');
+
+function sectionHandler(){
+    // window.screenY
+}
+
+let currentSection = section1;
+
+window.addEventListener('wheel', function(event){
+    if(event.deltaY > 0){   // 휠을 내린경우
+        if(currentSection == section1){
+            window.scrollTo({top: section2.offsetTop, behavior: 'smooth'});
+            currentSection = section2;
+        }else if(currentSection == section2){
+            window.scrollTo({top: section3.offsetTop, behavior: 'smooth'});
+            currentSection = section3;
+        }
+    }else{  // 휠을 올린경우
+        if(currentSection == section2){
+            window.scrollTo({top: section1.offsetTop, behavior: 'smooth'});
+            currentSection = section1;
+        }else if(currentSection == section3){
+            window.scrollTo({top: section2.offsetTop, behavior: 'smooth'});
+            currentSection = section2;
+        }
+    }
+});
+
+
+
+// scroll - animation 살짝 ~
+
+window.addEventListener('scroll', function(){
+    if(section2.getBoundingClientRect().top === 0){
+        // console.log(section2.children[0]);
+        section2.children[0].classList.add('text-animation');
+    }else{
+        section2.children[0].classList.remove('text-animation');
+    }
+});
